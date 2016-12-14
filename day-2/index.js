6cfc5ee6898bb05c567c365dd94cb3f0df2a00c2
Pavel Nasovich <forcewake@gmail.com>
2016-12-15 01:33:44 +0300

    Remove prism from index.js

--- day-2/index.js
+++ day-2/index.js
@@ -3,10 +3,4 @@ import { render } from "react-dom";
 
 import Presentation from "./presentation";
 
-import 'prismjs'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-handlebars'
-import 'prismjs/themes/prism-tomorrow.css'
-import './assets/styles/code.less'
-
 render(<Presentation/>, document.getElementById("root"));
